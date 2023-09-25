7fe218586ac753632e951f99ff4da65417f19d06
Mohammed Al wetaishi <mohamedsw2020@gmail.com>
2023-09-25 17:26:02 +0300

    adding a test case to Sorter.java File

--- src/package_1/Sorter.java
+++ src/package_1/Sorter.java
@@ -1,15 +1,19 @@
 package package_1;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sorter {
     public static void main(String args[]){
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0 ; i < 10 ; i++){
-            list.add((int)(Math.random()*9)+1);
+        for(int i=1 ; i <= 10 ; i++){
+            list.add(i);
         }
-        System.out.println(list);
+        Collections.shuffle(list);
+        
+        System.out.println("Before Sorting: "+ list);
         selectionSort(list);
-        System.out.println(list);
+        System.out.println("After Sorting: "+list);
+        
 
     }
 
